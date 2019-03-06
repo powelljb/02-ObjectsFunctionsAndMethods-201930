575b8250f0a104675845fcd14391275845d75406
powelljb <powelljb@rose-hulman.edu>
2019-03-06 09:09:54 -0500

    Done.

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Jake Powell.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -18,17 +18,28 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 ###############################################################################
 
 import rosegraphics as rg
-
+import math
 
 def main():
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
     """
+    answer = triangle(3,4)
+    print(answer)
+    #integer = 4
+    #string = 'blue'
+    #string_function(string, integer)
+    integer = 7
+    string = 'red'
+    string_function(string,integer)
+
+
+
 
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# DONE: 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -36,14 +47,18 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# DONE: 3b.  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
+def triangle(a,b):
+    return math.sqrt((a**2) + (b**2))
+
+
 
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -69,6 +84,21 @@ def main():
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
+
+def string_function(string, integer):
+    window = rg.TurtleWindow()
+    jake = rg.SimpleTurtle('triangle')
+    jake.pen = rg.Pen('green', integer)
+    mason = rg.SimpleTurtle()
+    mason.pen = rg.Pen(string, 5)
+    jake.pen_down()
+    jake.forward(100)
+    jake.pen_up()
+    mason.pen_down()
+    mason.backward(100)
+    mason.pen_up()
+
+    window.close_on_mouse_click()
 
 
 ###############################################################################
